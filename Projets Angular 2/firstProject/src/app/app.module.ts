2e849cb29c639b6e115437067fa7aa9ddaa503fc
Marie G <mgandrille@yahoo.fr>
2020-04-11 19:49:27 +0200

    Add Folders

---
+++ Projets Angular 2/firstProject/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksListComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
