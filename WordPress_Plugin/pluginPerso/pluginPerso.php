9b33fc69869fdc27a7ea3b9061960b293ea0a49a
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-06 13:45:33 +0200

    Add primary function in pluginPerso

--- WordPress_Plugin/pluginPerso/pluginPerso.php
+++ WordPress_Plugin/pluginPerso/pluginPerso.php
@@ -2,3 +2,21 @@
 /**
  * Plugin Name: pluginPerso
  */
+
+function activatedHook(){
+    error_log('Le plugingPerso est Activé, Houra !') ;
+}
+
+function desactivatedHook(){
+    error_log('Le plugingPerso est Désactivé, oh Zut !') ;
+
+}
+
+function uninstalHook(){
+    error_log('Le pluginPerso est Désinstallé, Dommage !') ;
+
+}
+
+register_activation_hook(__FILE__, 'activatedHook');
+register_deactivation_hook(__FILE__, 'desactivatedHook');
+register_uninstall_hook(__FILE__, 'uninstalHook');
