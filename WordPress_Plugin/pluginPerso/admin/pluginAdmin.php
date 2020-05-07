712b6db979e1edcb0d30730bd9344ac295fefc7b
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-07 11:06:58 +0200

    Add Page menu du plugin

---
+++ WordPress_Plugin/pluginPerso/admin/pluginAdmin.php
@@ -0,0 +1,10 @@
+<?php
+
+register_setting( $option_group:string, $option_name:string, $args:array )
+
+?>
+
+<div class="wrap">
+    <h1>Page d'adiministration de mon plugin</h1>
+    <h2>pluginPerso</h2>
+</div>
--- WordPress_Plugin/pluginPerso/pluginPerso.php
+++ WordPress_Plugin/pluginPerso/pluginPerso.php
@@ -6,17 +6,32 @@
 function activatedHook(){
     error_log('Le plugingPerso est Activé, Houra !') ;
 }
+register_activation_hook(__FILE__, 'activatedHook');
 
 function desactivatedHook(){
     error_log('Le plugingPerso est Désactivé, oh Zut !') ;
-
 }
+register_deactivation_hook(__FILE__, 'desactivatedHook');
 
 function uninstalHook(){
     error_log('Le pluginPerso est Désinstallé, Dommage !') ;
+}
+register_uninstall_hook(__FILE__, 'uninstalHook');
 
+
+// Création de la page d'administration dans le menu
+
+function pluginPerso_addmenu() {
+    error_log("ajout du menu...");
+    add_menu_page(
+        'plugingPerso admin',
+        'Mon Plugin',
+        'manage_options',
+        plugin_dir_path(__FILE__) . 'admin/pluginAdmin.php',
+        null,
+        'dashicons-admin-tools',
+        20
+    );
 }
 
-register_activation_hook(__FILE__, 'activatedHook');
-register_deactivation_hook(__FILE__, 'desactivatedHook');
-register_uninstall_hook(__FILE__, 'uninstalHook');
+add_action( 'admin_menu', 'pluginPerso_addmenu');
