277eec7c0349830a23ff7270078ef1899bf43a8a
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-11 17:15:57 +0200

    Amélioration de la premiere base

--- Projets AJAX/js/scripts.js
+++ Projets AJAX/js/scripts.js
@@ -20,9 +20,16 @@
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
                 let villes = JSON.parse(httpRequest.responseText);
-                console.log(villes);
-                console.log(villes[0].nom);
+                console.log(httpRequest.response);
+                // console.log(villes);
+                // console.log(villes[0].nom);
                 alert(villes[0].nom);
+
+                let ville = villes[0];
+                for(let i in ville) {
+                    console.log(i);
+                    console.log(ville[i]);
+                }
 
             } else {
                 alert('Il y a eu un problème avec la requête.');
