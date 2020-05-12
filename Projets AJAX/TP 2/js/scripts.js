7f2a9e830c33f3fd09bdafa95c772e18fc686d0c
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-12 11:08:14 +0200

    fin TP2 (avec toutes les villes)

--- Projets AJAX/TP 2/js/scripts.js
+++ Projets AJAX/TP 2/js/scripts.js
@@ -1,8 +1,24 @@
-(function() {
-    $("#ajaxButton").on("click", function () {
-        let codePostal = $("#codePostal").val();
-        $.get(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}`, function (data) {
-            alert(JSON.stringify(data));
-        });
-    });  
-}) ();
+$.noConflict();
+jQuery(document).ready(function($) {
+
+    (function() {
+        $("#ajaxButton").on("click", function () {
+            let codePostal = $("#codePostal").val();
+            $.get(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}`, function (villes) {
+                console.log(villes);
+
+                let nomVilles = "";
+                for(let i in villes) {
+                    console.log(i);
+                    console.log(villes[i].nom);
+                    nomVilles = nomVilles + villes[i].nom + " , " ;
+                }
+
+                alert(JSON.stringify(nomVilles));
+            });
+        });  
+    }) ();
+
+
+
+}
