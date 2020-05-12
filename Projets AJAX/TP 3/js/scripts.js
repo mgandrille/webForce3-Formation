624f7e026dbd840ab52024dbb1008321e4bba49c
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-12 13:59:53 +0200

    TP3 : amélioration

--- Projets AJAX/TP 3/js/scripts.js
+++ Projets AJAX/TP 3/js/scripts.js
@@ -7,14 +7,18 @@ jQuery(document).ready(function($) {
             $.get(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}`, function (villes) {
                 // console.log(villes);
 
-                $('#resultat').replaceWith('<div id="resultat"> </div>')
-                $('<ul id="listeVilles">').appendTo('#resultat');
-                $.each(villes, (i, ville) =>{
-                    console.log(i);
-                    console.log(ville.nom);
-                    $('#listeVilles').append('<li>' + ville.nom + '</li>');
-                });
-                $('#resultat').append('</ul>');
+                if(Date.lenght != 0) {
+                    $('#resultat').replaceWith('<div id="resultat"> </div>')
+                    $('<ul id="listeVilles">').appendTo('#resultat');
+                    $.each(villes, (i, ville) =>{
+                        // console.log(i);
+                        console.log(ville.nom);
+                        $('#listeVilles').append('<li>' + ville.nom + '</li>');
+                    });
+                    $('#resultat').append('</ul>');
+                } else {
+                    alert('Pas de résultat !')
+                }
 
             });
         });  
