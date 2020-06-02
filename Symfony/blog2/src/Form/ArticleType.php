7b960dc0cf03721e3bbe594d97c7a68abe400735
Marie Gandrille <mgandrille@yahoo.fr>
2020-06-02 11:27:14 +0200

    Mise en page Form Article

--- Symfony/blog2/src/Form/ArticleType.php
+++ Symfony/blog2/src/Form/ArticleType.php
@@ -6,17 +6,40 @@ use App\Entity\Article;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('created_at')
+            ->add('title', TextType::class, [
+                'label' => 'Titre de votre article',
+                'data' => 'Titre par défaut',
+                'attr' => [
+                    'class' => 'form-control-sm form-red'
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Your title should be at least {{ limit }} characters',
+                        'max' => 255,
+                        'maxMessage' => 'Your title should be maximum {{ limit }} characters',
+                    ]),
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu de votre article',
+                'data' => 'Contenu par défaut'
+            ])
+            ->add('created_at', DateType::class,  [
+                'widget' => 'single_text'
+            ])
             ->add('short_content')
             ->add('category', EntityType::class, [
                 'class' => Category::class, // Quelle classe est reliée au champ category
