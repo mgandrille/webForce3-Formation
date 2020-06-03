67d753ea316f99d2292da15a7e9fb7796898b568
Marie Gandrille <mgandrille@yahoo.fr>
2020-06-03 10:56:23 +0200

    NotaResto : Add new 'Fake' restau&reviews

--- Symfony/NotaResto/src/DataFixtures/RestaurantFixtures.php
+++ Symfony/NotaResto/src/DataFixtures/RestaurantFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Restaurant;
+use App\Repository\CityRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class RestaurantFixtures extends Fixture
 {
+    private $cityRepository;
+
+    public function __construct(CityRepository $cityRepository) {
+        $this->cityRepository = $cityRepository;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for($i=0; $i < 1000; $i++) {
+            $restaurant = new Restaurant();
+            $restaurant->setName( $faker->company );
+            $restaurant->setDescription( $faker->text(500) );
+            $restaurant->setCity( $this->cityRepository->find( rand(1, 1000) ) );
+
+            $manager->persist($restaurant);
+        }
 
         $manager->flush();
     }
